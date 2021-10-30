26a71c4597395b7b505b823cc9c0e05bbcb3b4a2
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-30 20:10:15 +0600

    loading all orders of all the users on UI from database through server

--- src/components/Admin/ManageAllOrders/ManageAllOrders.js
+++ src/components/Admin/ManageAllOrders/ManageAllOrders.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Card, Col, Button, Table } from 'react-bootstrap';
+import useAuth from '../../../hooks/useAuth';
+import Header from '../../Shared/Header/Header';
+import Footer from '../../Shared/Footer/Footer';
 
 const ManageAllOrders = () => {
+
+    const { userInfo } = useAuth();
+
+    const [orders, setOrders] = useState([]);
+
+    const userEmail = userInfo?.email;
+
+    useEffect(() => {
+        fetch('http://localhost:5000/orders')
+            .then(res => res.json())
+            .then(data => {
+                setOrders(data)
+                console.log(orders)
+            })
+    }, [orders])
+
+    const handleUpdate = id => {
+        console.log(id);
+    }
+
     return (
         <div>
+            <Header></Header>
+            <div className="container mt-5 mb-5" id="" style={{ maxWidth: '100vw' }}>
+                <h1 className="text-center mb-4">My Orders</h1>
+                <h4 className="text-center mb-5 text-secondary">See all of your orders here</h4>
+                <h2 className="text-center mb-5 ">Number of Orders: {orders?.length}</h2>
+                {
+                    <div>
+                        <Table responsive="sm">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Service Name</th>
+                                    <th>Service Price</th>
+                                    <th>User's Email</th>
+                                    <th>Order Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    orders?.map((order, index) => {
 
+                                        return <tr>
+                                            <td>{index}</td>
+                                            <td>{order?.serviceName}</td>
+                                            <td>{order?.price}</td>
+                                            <td>{order?.email}</td>
+                                            <td>{order?.status}</td>
+                                            <td>  <Button variant="primary" onClick={() => handleUpdate(order?._id)}>Update Status</Button>
+                                            </td>
+
+                                        </tr>
+                                    })
+                                }
+
+                            </tbody>
+                        </Table>
+                    </div>
+                }
+
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
